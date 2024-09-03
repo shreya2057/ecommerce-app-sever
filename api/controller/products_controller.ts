2020979fbad229056d5415f5947be8464b97e808
shreya <shreyashrestha2k57@gmail.com>
2024-09-03 21:24:36 +0545

    fix: validation in products post api

--- api/controller/products_controller.ts
+++ api/controller/products_controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { Product } from "../model/products";
 import cloudinary from "../config/cloudinary";
+import { Categories } from "../model/categories";
+import mongoose from "mongoose";
 
 const post_products = async (req: Request, res: Response) => {
   try {
@@ -13,20 +15,31 @@ const post_products = async (req: Request, res: Response) => {
       }
     );
 
-    const request = new Product({
-      ...req?.body,
-      image: cloudinaryResult?.secure_url,
-      category: req.params.category_id,
-    });
+    const ObjectId = mongoose.Types.ObjectId;
 
-    await request.save();
-    res.json({
-      message: "Products created successfully",
-      status: 200,
-      data: request,
+    const findCategory = await Categories.findOne({
+      _id: new ObjectId(req.params.category_id),
     });
+    if (!!findCategory) {
+      const request = new Product({
+        ...req?.body,
+        image: cloudinaryResult?.secure_url,
+        category: req.params.category_id,
+      });
+
+      await request.save();
+      res.json({
+        message: "Products created successfully",
+        status: 200,
+        data: request,
+      });
+    } else {
+      res.json({
+        message: "Category instance not found",
+        status: 400,
+      });
+    }
   } catch (e) {
-    console.log(e);
     res.json({ message: "Internal server error", status: 500 });
   }
 };
