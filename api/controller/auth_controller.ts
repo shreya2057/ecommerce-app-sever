48af8dc1b09b61b91e0f04c22b421267e44919f7
shreya <shreya.shrestha@codehimalaya.net>
2024-10-11 23:03:58 +0545

    fix: error display

--- api/controller/auth_controller.ts
+++ api/controller/auth_controller.ts
@@ -113,6 +113,7 @@ export const login_controller = async (req: Request, res: Response) => {
       },
     });
   } catch (e) {
+    console.log(e);
     res.json({ message: "Internal server error", status: 500 });
   }
 };
